a6ced5cf095edeeb4300a6d51895840e720323f6
Carolina Carranza <cmcc1505@gmail.com>
2021-10-03 00:28:08 -0600

    Add final section and script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const EventListeners = () => {
+    const done = document.getElementById('#done');
+    done.setAttribute('href', 'https://hencor2019.github.io/nvm-guide/#1')
+};
+
+const app = () => {
+    EventListeners();
+}
+
+window.onload = app;
+
+

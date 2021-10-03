e68d86a353df3c897b42e13ed01bee8dd8d07b81
Carolina Carranza <cmcc1505@gmail.com>
2021-10-03 01:12:42 -0600

    Update: Redirection to menu when back arrow is clicked

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const eventListeners = () => {
     const done = document.getElementById('done');
-    done.setAttribute('href', 'https://hencor2019.github.io/nvm-guide/#1')
+    const back = document.getElementById('arrow-back');
+    done.setAttribute('href', 'https://hencor2019.github.io/nvm-guide/#1');
+    back.setAttribute('href', 'https://hencor2019.github.io/nvm-guide/#1');
 };
 
 const app = () => {
